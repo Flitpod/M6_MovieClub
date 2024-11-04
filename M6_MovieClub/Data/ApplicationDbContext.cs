3c161c7b7d58de984988e8320b6fa0e8c571429c
TothDavid <130594869+Flitpod@users.noreply.github.com>
2024-11-04 15:53:38 +0200

    Add project files.

---
+++ M6_MovieClub/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace M6_MovieClub.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
